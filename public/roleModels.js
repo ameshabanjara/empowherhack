e955138544392a033cbe4052f962d64c11482121
Amesha Banjara <abanjara@stanford.edu>
2024-07-21 13:29:11 -0500

    Create roleModels.js

---
+++ public/roleModels.js
@@ -0,0 +1,39 @@
+export const models = [
+    {
+        id: 1,
+        name: "parisa tabriz",
+        image: "/image_parisatabriz.png",
+        blurb: "parisa tabriz is known as the google “security princess” and holds the title to show the world that women belong in the cybersecurity realm. she is the director of engineering at google managing the chrome web browser, the world’s most popular browser! currently she is working on how to keep search queries and other data private even when multiple users are sharing a single phone for internet access as it often is in the developing world. she graduated with a bachelors and masters in computer science from university of illinois urbana-champaign."
+    },
+    {
+        id: 2,
+        name: "kate moussouris",
+        image: "/image_katemoussouris.png",
+        blurb: "kate moussouris is the founder of luta security and is a pioneer in vulnerability disclosure and bug bounties (she led for first bug bounty programs for the us government and microsoft!), she has significantly contributed to shaping the current cybersecurity landscape. "
+    },
+
+    {
+        id: 3,
+        name: "kathleen mckeown",
+        image: "/image_kathleenmckeown.png",
+        blurb: "kathleen kckeown is a professor of computer science at columbia university and a pioneering researcher in the field of natural language processing. she has made significant contributions to text summarization, natural language generation, and information extraction. she has received numerous awards for her research, including being named a fellow of the association for computational linguistics."
+    },
+    {
+        id: 4,
+        name: "raquel urtasun",
+        image: "/image_raquelurtasun.png",
+        blurb: "raquel urtasun is the founder and ceo of waabi, a company focused on developing next-generation self-driving technology. she is also a professor of computer science at the university of toronto and a former chief scientist at uber atg. her research in computer vision and machine learning has been fundamental to the development of autonomous driving technologies."
+    },
+    {
+        id: 5,
+        name: "kirsten kliphouse",
+        image: "/image_kirstenkliphouse.png",
+        blurb: "kirsten kliphouse is president at google cloud. she leads both the customer-facing and the partner-facing teams with a vast variety of customers and partners - being small to midsize businesses, and mid-market clients."
+    },
+    {
+        id: 6,
+        name: "nive bhagat",
+        image: "/image_nivebhagat.png",
+        blurb: "nive bhagat is the global chief executive officer (ceo) for cloud & infrastructure services and a member of the group executive officer for capgemini, which is an international it services and consulting company based in france. bhagat has an established history of creating strategic visions for businesses to grow and turn their tracks around. "
+    }
+]
