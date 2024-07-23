5add51c5778d18788d7c94aa5534d01f99d7ad98
Elizabeth Yan <161661526+yntianyu@users.noreply.github.com>
2024-07-23 01:31:29 -0400

    Create page.js

---
+++ app/careers/page.js
@@ -0,0 +1,10 @@
+import "../globals.css"
+import React from "react";
+import CareerDetails from "@/components/CareerDetails";
+import CareersList from "@/components/Careers";
+
+export default function Careers() {
+    return (
+      <CareersList />
+    );
+  };
