481bfa318aeb9c446778ad5b4f08c887ceb50f42
Elizabeth Yan <161661526+yntianyu@users.noreply.github.com>
2024-07-23 01:42:02 -0400

    Create page.js

---
+++ app/whowelookupto/page.js
@@ -0,0 +1,9 @@
+import "../globals.css"
+import React from "react";
+import WhoWeLookUpToList from "@/components/WhoWeLookUpTo";
+
+export default function WhoWeLookUpTo() {
+    return (
+      <WhoWeLookUpToList />
+    );
+  };
