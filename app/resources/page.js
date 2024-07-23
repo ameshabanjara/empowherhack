a9b86bde74a7ff9420021eda0e67473df2a58d79
Elizabeth Yan <161661526+yntianyu@users.noreply.github.com>
2024-07-23 01:31:52 -0400

    Create page.js

---
+++ app/resources/page.js
@@ -0,0 +1,9 @@
+import "../globals.css"
+import React from "react";
+import ResourcesList from "@/components/Resources";
+
+export default function Careers() {
+    return (
+      <ResourcesList />
+    );
+  };
